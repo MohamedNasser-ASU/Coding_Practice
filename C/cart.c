39aa2c5134d21d7eeae2a844ffeb91ee4237f9d2
Mohamed Nasser <mohamednasser12018@gmail.com>
2026-08-27 10:05:47 +0300

    wrote first function

---
+++ C/cart.c
@@ -0,0 +1,13 @@
+// practice 1, shopping cart.
+#include <stdio.h>
+
+int main(void){
+    
+    char word[128];
+    printf("What item would you like to buy? ");
+    scanf(" %s", word);
+    printf( "%s" , word);
+
+
+    return 0;
+}
