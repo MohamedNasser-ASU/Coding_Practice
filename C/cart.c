1ed2e25c182ba4172735a16b77ccb04ae781680b
Mohamed Nasser <mohamednasser12018@gmail.com>
2026-08-27 10:16:53 +0300

    wrote second function

--- C/cart.c
+++ C/cart.c
@@ -1,13 +1,18 @@
 // practice 1, shopping cart.
+
 #include <stdio.h>
 
 int main(void){
     
     char word[128];
+    float price;
+    int qty;
+
     printf("What item would you like to buy? ");
     scanf(" %s", word);
-    printf( "%s" , word);
-
+    
+    printf("What is the price for each? ");
+    scanf("%f", &price);      
 
     return 0;
 }
